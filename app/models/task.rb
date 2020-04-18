d3f312ca9151afa2e10ef3a5344d5eeb2d757c19
Zakir Hussain <zack.mesacc@gmail.com>
2020-04-19 03:38:48 +0500

    Style task components and create partials

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,12 @@
 class Task < ApplicationRecord
   belongs_to :user
   validates :content, presence: true, length: { minimum: 5, maximum: 200 }
+  
+  auto_html_for :content do
+    html_escape
+    image
+    youtube(width: '100%', height: 200)
+    link target: '_blank', rel: 'nofollow' 
+    simple_format
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_17_130043) do
+ActiveRecord::Schema.define(version: 2020_04_18_193143) do
 
   create_table "tasks", force: :cascade do |t|
     t.text "content"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_04_17_130043) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.string "status", default: "to-do"
+    t.text "content_html"
   end
 
   create_table "users", force: :cascade do |t|
